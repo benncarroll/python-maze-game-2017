e9365004d0c136629ec48148cb3bf14bd64d9545
Ben Carroll <ben.carroll@student.tsc.nsw.edu.au>
2017-09-11 09:44:46 +1000

    Huge commit, lots of features and ease of use improvements
    + Enemies!
       • Enemies will move around the maze, moving on their own accord and only turning when they hit walls
       • When an enemy comes into contact with the player, the player dies
       • Enemies can go through gates
    + Reset and get_pos_tuple methods added to both enemy and player class
    + Added intro message, asking if they want/need to resize their terminal
    + Added ability for player to die, with checks every turn
    + Added some comments here and there
    
    • Play area is always centred now, regardless of terminal size
    • Loop ticks by default now, done by adding a timeout to the screen which then applies to the .getch() method

--- classes.py
+++ classes.py
@@ -4,8 +4,8 @@
 from maze import *
 import random
 
-
 # Player Class
+
 
 class Player(object):
     """docstring for Player."""
@@ -17,6 +17,13 @@ class Player(object):
         self.prev = (1, 4)
         self.keys = 0
         self.icon = "⇉"
+
+    def reset(self):
+        self.pos = (1, 3)  # (Y,X)
+        self.prev = (1, 4)
+        self.keys = 0
+        self.icon = "⇉"
+        pass
 
     def give_key(self):
         self.keys += 1
@@ -77,3 +84,73 @@ class Player(object):
             return a[0]
         else:
             return a[1]
+
+    def get_pos_tuple(self, p=0):
+        if p == 0:
+            return self.pos
+        else:
+            return self.prev
+
+# Enemy Class
+
+
+class Enemy(object):
+    """docstring for Enemy."""
+
+    def __init__(self, y, x):
+        super(Enemy, self).__init__()
+
+        self.initargs = (y, x)
+        self.pos = (y, x)  # (Y,X)
+        self.prev = (y, x + 1)
+        self.icon = "✧"
+        self.dir = (0, 1)
+
+    def reset(self):
+        self.pos = self.initargs  # (Y,X)
+        self.prev = (10, 32)
+
+    def move(self, lmaze):
+
+        special_items = ["█"]  # , "‖", "="]  # , "⥉", "⊙"]
+        dir_list = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+
+        # Random movement
+
+        random.shuffle(dir_list)
+
+        if lmaze[self.pos[0] + self.dir[0]][self.pos[1] + self.dir[1]] in special_items:
+            for i in dir_list:
+                if lmaze[self.pos[0] + i[0]][self.pos[1] + i[1]] not in special_items:
+                    self.prev = self.pos
+                    self.pos = (self.pos[0] + i[0], self.pos[1] + i[1])
+                    self.dir = i
+                    break
+        else:
+            self.prev = self.pos
+            self.pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
+        return
+
+    def set_icon(self, i):
+        self.icon = i
+        pass
+
+    def get_icon(self):
+        return self.icon
+
+    def get_pos(self, v, p=0):
+        if p == 0:
+            a = self.pos
+        else:
+            a = self.prev
+
+        if v == 0:
+            return a[0]
+        else:
+            return a[1]
+
+    def get_pos_tuple(self, p=0):
+        if p == 0:
+            return self.pos
+        else:
+            return self.prev
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
+# Import Dependencies
 import sys
+import subprocess
 import curses
 from time import sleep
 
@@ -10,49 +11,169 @@ from time import sleep
 from classes import *
 from maze import *
 
-# Globals
+
+##############################
+##      Size checking.      ##
+##############################
+
+# Get dimensions of Terminal
+tr, tc = subprocess.check_output(['stty', 'size']).decode().split()
+ts = True
+while ts:
+    # Define inside loop so that it is run once.
+    ts = True if int(tr) < 24 or int(tc) < 80 else False
+    if ts:
+        msg = """
+   Your terminal size is
+         too small.
+
+  It needs to be at least
+     80 cols x 24 rows.
+
+     Please resize your
+        terminal and
+        press enter.
+"""
+    else:
+        msg = """
+   Would you like to play
+      the game in it's
+       optimal size?
+"""
+
+    print("""
+-----------------------------
+{}
+   To resize automatically,
+    type 'auto', followed
+         by enter.
+
+-----------------------------
+""".format(msg))
+
+    if input("") == 'auto':
+        ts_scpt = """tell application "Terminal"
+            set bounds of front window to {0, 0, 1920, 1080}
+        end tell
+
+        tell application "System Events"
+            keystroke "0" using {command down}
+            repeat 14 times
+                key code 69 using {command down}
+            end repeat
+        end tell"""
+
+        # Run script to resize terminal to desired dimensions
+        proc = subprocess.Popen(['osascript', '-'],
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True)
+        stdout_output = proc.communicate(ts_scpt)[0]
+
+    tr, tc = subprocess.check_output(['stty', 'size']).decode().split()
+    ts = True if int(tr) < 24 or int(tc) < 80 else False
+
+# Clear terminal
+print("\n" * 100)
+
+
+##############################
+##     Global Variables.    ##
+##############################
+
 lmaze = list()
 p = Player()
+e = [Enemy(1, 50), Enemy(7, 35), Enemy(3, 72)]
 top_msg = ""
 special_items = ["⥉", "‖", "⊙"]
 
 
-def start_game(screen):
+##############################
+##       Title Screen.      ##
+##############################
+
+def start_game(screen, d=0):
+
+    # Get dims of terminal with curses
     dims = screen.getmaxyx()
 
-    l1 = "Welcome to Carroll's CYOA Game."
-    l2 = "Press any key to start."
-    l3 = "Press Q at any time to quit."
+    # Added to clear previous game if player has died.
+    screen.clear()
 
+    # Change messages dependent on death
+    if d == 0:
+        l1 = "Welcome to Carroll's CYOA Game."
+        l2 = "Press any key to start."
+        l3 = "Press Q at any time to quit."
+    else:
+
+        # Set timeout to infinite so title screen is not skipped.
+        screen.timeout(-1)
+
+        # Reset enemies & players
+        for enemy in e:
+            enemy.reset()
+        p.reset()
+
+        l1 = "You died."
+        l2 = "Press any key to start a new game."
+        l3 = " "
+
+    # Print title screen lines
     screen.addstr(dims[0] // 2 - 2, dims[1] // 2 - (len(l1) // 2), l1, curses.color_pair(3) | curses.A_BOLD)
     screen.addstr(dims[0] // 2 + 0, dims[1] // 2 - (len(l3) // 2), l3, curses.color_pair(3) | curses.A_BOLD)
     screen.addstr(dims[0] // 2 + 2, dims[1] // 2 - (len(l2) // 2), l2, curses.color_pair(4) | curses.A_STANDOUT)
 
+    # Update lines
     screen.refresh()
 
+    z = 2 if d == 1 else 0
+    sleep(z)
+
+    # Wait for user's input to pass title screen
     key = screen.getch()
     if key == 113 or key == 81:
-        sys.exit("-----  Game quit.  -----")
+        sys.exit("-" * 33 + "  Game quit.  " + "-" * 33)
+
+    # Read in maze from file.
+    del lmaze[:]
+    pmaze = [y for y in (x.strip() for x in fmaze.splitlines()) if y]
+    for i in pmaze:
+        lmaze.append(list(i))
+
+##############################
+##     Print Play Area.     ##
+##############################
 
 
 def print_map(screen):
+
+    screen.clear()
+
     dims = screen.getmaxyx()
 
     for i, content in enumerate(lmaze):
         string = ''.join(content)  # + "\n"
-        screen.addstr(i, 0, string, curses.color_pair(6))
+        screen.addstr(dims[0] // 2 - 12 + i, dims[1] // 2 - (len(string) // 2), string, curses.color_pair(6))
+
+    # Show spaces:
+    for enemy in e:
+        screen.addstr(dims[0] // 2 - 12 + enemy.get_pos(0, 1), dims[1] // 2 - 40 + enemy.get_pos(1, 1), ' ',              curses.color_pair(0))
+    screen.addstr(dims[0] // 2 - 12 + p.get_pos(0, 1), dims[1] // 2 - 40 + p.get_pos(1, 1), ' ', curses.color_pair(0))
+    lmaze[p.get_pos(0, 1)][p.get_pos(1, 1)] = " "
 
     # Colorise items
     for r, row in enumerate(lmaze):
         for c, column in enumerate(row):
             if column in special_items:
-                screen.addstr(r, c, column, curses.color_pair(4))
-
+                screen.addstr(dims[0] // 2 - 12 + r, dims[1] // 2 - 40 + c, column, curses.color_pair(4))
 
     # Display player's position
-    screen.addstr(p.get_pos(0, 1), p.get_pos(1, 1), ' ', curses.color_pair(0))
-    screen.addstr(p.get_pos(0), p.get_pos(1), p.get_icon(), curses.color_pair(2))
-    lmaze[p.get_pos(0, 1)][p.get_pos(1, 1)] = " "
+    screen.addstr(dims[0] // 2 - 12 + p.get_pos(0), dims[1] // 2 - 40 + p.get_pos(1), p.get_icon(), curses.color_pair(3) | curses.A_BOLD)
+
+    # Display enemies
+    for enemy in e:
+        screen.addstr(dims[0] // 2 - 12 + enemy.get_pos(0),  dims[1] // 2 - 40 + enemy.get_pos(1),    enemy.get_icon(), curses.color_pair(2))
 
     # Show top_msg
     screen.addstr(0, dims[1] // 2 - (len(top_msg) // 2), top_msg, curses.color_pair(1))
@@ -75,11 +196,6 @@ def main(screen):
 
     # Intro
     start_game(screen)
-
-    # Read in maze from file once.
-    pmaze = [y for y in (x.strip() for x in fmaze.splitlines()) if y]
-    for i in pmaze:
-        lmaze.append(list(i))
 
     # Print map
     print_map(screen)
@@ -109,6 +225,11 @@ def main(screen):
             p.set_icon("⇇")
             top_msg = p.move(top_msg, lmaze, -1)
 
+        # Update enemy positions
+        for enemy in e:
+            enemy.move(lmaze)
+
+        # Hide notifications after 10 moves.
         if msg_count >= 10 and top_msg == old_msg:
             msg_count = 0
             top_msg = ""
@@ -117,13 +238,18 @@ def main(screen):
         elif top_msg != old_msg:
             msg_count = 0
 
-        print_map(screen)
-        # if key == 112:
-        # screen.addstr(20,0, "Keys: {} top_msg: {}".format(p.keys, top_msg[:50]))
+        # Check death
+        for i in e:
+            if i.get_pos_tuple() == p.get_pos_tuple():
+                start_game(screen, 1)
 
+        print_map(screen)
+
+        screen.timeout(100)
         key = screen.getch()
+
         if key == 113 or key == 81:
-            sys.exit("Game quit.")
+            sys.exit("-" * 33 + "  Game quit.  " + "-" * 33 + "\n\n\n")
 
 
 curses.wrapper(main)
